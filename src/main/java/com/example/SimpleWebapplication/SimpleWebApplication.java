371fcd93ed2854fceb48779c6cb54ddecd351f05
shahid803 <maliksahidmaliksiddiq@gmail.com>
2021-07-24 16:03:27 +0530

    initial commit

--- src/main/java/com/example/SimpleWebapplication/SimpleWebApplication.java
+++ src/main/java/com/example/SimpleWebapplication/SimpleWebApplication.java
@@ -1,13 +1,7 @@
 package com.example.SimpleWebapplication;
 
-import org.apache.catalina.core.ApplicationContext;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import java.util.Enumeration;
-
 
 @SpringBootApplication
 public class SimpleWebApplication {
